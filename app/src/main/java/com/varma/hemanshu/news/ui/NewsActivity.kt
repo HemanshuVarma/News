7093605151900be21f20663dacd1291870105116
Hemanshu Varma <hemanshuvarma916@gmail.com>
2022-07-02 22:42:50 +0530

    Add: Bottom navigation

--- app/src/main/java/com/varma/hemanshu/news/ui/NewsActivity.kt
+++ app/src/main/java/com/varma/hemanshu/news/ui/NewsActivity.kt
@@ -2,11 +2,17 @@ package com.varma.hemanshu.news.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.varma.hemanshu.news.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        //Coupling bottom navigation view with navigation graph(it's controller)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
